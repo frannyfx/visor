8465a3fb4c8f140a05b6f0badc0266550da77d44
Francesco Compagnoni <compfrancesco@gmail.com>
2020-04-23 15:17:30 +0100

    Add more detailed logging on the injection process.

--- Injector/main.cpp
+++ Injector/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include "util/process.h"
+#include <Windows.h>
+
+string processName = "notepad.exe";
 
 int main()
 {
-    int pid = GetPIDWithName("notepad.exe");
-    InjectLibrary(pid, "C:\\Users\\spydas\\source\\repos\\visor\\x64\\Debug\\Core.dll");
+    // Get running directory
+    char filename[MAX_PATH];
+    GetModuleFileNameA(0, filename, sizeof(filename));
+    string filenameStr = (string)filename;
+    string parentDirectory = filenameStr.substr(0, filenameStr.find_last_of("\\"));
+
+    // Get the PID of the process
+    int pid = GetPIDWithName(processName);
+    if (pid == 0) {
+        cerr << "Could not get handle on process " << processName << "\." << endl;
+        return 1;
+    }
+
+    cout << "Found process " << processName << " (" << pid << ")." << endl;
+
+    // Get library path and inject
+    string libraryPath = parentDirectory + "\\Core.dll";
+    cout << "Attempting to inject library " << libraryPath << " into " << processName << " (" << pid << ")." << endl;
+    InjectLibrary(pid, libraryPath);
     return 0;
 }
