44be87ad7a7141ce508236a3dbc84adbc369c209
Neriuzz <neriusilmonas@gmail.com>
2020-04-24 17:53:54 +0100

    Add simple ImGui overlay

--- Core/Hook/APIs/D3D9.cpp
+++ Core/Hook/APIs/D3D9.cpp
@@ -23,6 +23,8 @@
 
 using namespace std;
 
+static void ShowExampleAppSimpleOverlay(bool* p_open);
+
 namespace D3D9Hook {
 	// Hooking
 	typedef HRESULT(__stdcall* D3D9EndSceneHook) (LPDIRECT3DDEVICE9 pDevice, const RECT* pSourceRect, const RECT* pDestRect, HWND hDestWindowOverride, const RGNDATA* pDirtyRegion);
@@ -55,7 +57,7 @@ namespace D3D9Hook {
 		ImGui::NewFrame();
 
 		bool show_demo_window = true;
-		ImGui::ShowDemoWindow(&show_demo_window);
+		ShowExampleAppSimpleOverlay(&show_demo_window);
 		ImGui::Render();
 		ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
 
@@ -123,4 +125,38 @@ namespace D3D9Hook {
 		detour.hook();
 #endif
 	}
+}
+
+static void ShowExampleAppSimpleOverlay(bool* p_open)
+{
+	const float DISTANCE = 10.0f;
+	static int corner = 0;
+	ImGuiIO& io = ImGui::GetIO();
+	if (corner != -1)
+	{
+		ImVec2 window_pos = ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE, (corner & 2) ? io.DisplaySize.y - DISTANCE : DISTANCE);
+		ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
+		ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+	}
+	ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+	if (ImGui::Begin("Example: Simple overlay", p_open, (corner != -1 ? ImGuiWindowFlags_NoMove : 0) | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav))
+	{
+		ImGui::Text("Simple overlay\n" "in the corner of the screen.\n" "(right-click to change position)");
+		ImGui::Separator();
+		if (ImGui::IsMousePosValid())
+			ImGui::Text("Mouse Position: (%.1f,%.1f)", io.MousePos.x, io.MousePos.y);
+		else
+			ImGui::Text("Mouse Position: <invalid>");
+		if (ImGui::BeginPopupContextWindow())
+		{
+			if (ImGui::MenuItem("Custom", NULL, corner == -1)) corner = -1;
+			if (ImGui::MenuItem("Top-left", NULL, corner == 0)) corner = 0;
+			if (ImGui::MenuItem("Top-right", NULL, corner == 1)) corner = 1;
+			if (ImGui::MenuItem("Bottom-left", NULL, corner == 2)) corner = 2;
+			if (ImGui::MenuItem("Bottom-right", NULL, corner == 3)) corner = 3;
+			if (p_open && ImGui::MenuItem("Close")) *p_open = false;
+			ImGui::EndPopup();
+		}
+	}
+	ImGui::End();
 }
