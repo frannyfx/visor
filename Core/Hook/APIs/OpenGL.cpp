c536485835d02c2d46a2acfb6afc0aa213f92b81
Neriuzz <neriusilmonas@gmail.com>
2020-04-25 15:34:50 +0100

    Switching over to MinHook

--- Core/Hook/APIs/OpenGL.cpp
+++ Core/Hook/APIs/OpenGL.cpp
@@ -5,13 +5,7 @@
 #include "OpenGL.h"
 
 // Hooking
-#include <polyhook2/CapstoneDisassembler.hpp>
-
-#ifdef _WIN64
-#include <polyhook2/Detour/x64Detour.hpp>
-#else
-#include <polyhook2/Detour/x86Detour.hpp>
-#endif
+#include "../../Include/MinHook/MinHook.h"
 
 // GUI
 #include "../../Include/ImGui/imgui.h"
@@ -21,32 +15,18 @@
 
 using namespace std;
 
-typedef HRESULT(__stdcall* OpenGLSwapBuffersHook)();
-uint64_t swapBuffersTrampoline = NULL;
-bool swapBuffersCalled = false;
+typedef BOOL(__stdcall* owglSwapBuffers)(HDC);
+// Trampoline function
+owglSwapBuffers twglSwapBuffers = NULL;
 
-HRESULT __stdcall swapBuffersHook() {
-	cout << "Kush" << endl;
-
-	return PLH::FnCast(swapBuffersTrampoline, swapBuffersHook)();
+BOOL __stdcall hkwglSwapBuffers(HDC hdc) {
+	cout << "Hooked" << endl;
+	return twglSwapBuffers(hdc);
 }
 
 void InitialiseOpenGLHooks() {
 	cout << "Installing hooks for OpenGL..." << endl;
+	if (MH_Initialize() != MH_OK) return;
 
-	// Setup ImGui
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	ImGui::StyleColorsDark();
-
-#ifdef _WIN64
-	PLH::CapstoneDisassembler disassembler(PLH::Mode::x64);
-	PLH::x64Detour detour((uint64_t)GetProcAddress(GetModuleHandle(TEXT("opengl32.dll")), "wglSwapBuffers"), (uint64_t)swapBuffersHook, &swapBuffersTrampoline, disassembler);
-	detour.hook();
-#else
-	PLH::CapstoneDisassembler disassembler(PLH::Mode::x86);
-	PLH::x86Detour detour((uint64_t)GetProcAddress(GetModuleHandle(TEXT("opengl32.dll")), "wglSwapBuffers"), (uint64_t)swapBuffersHook, &swapBuffersTrampoline, disassembler);
-	detour.hook();
-#endif
+	if (MH_CreateHook(GetProcAddress(GetModuleHandle(TEXT("opengl32.dll")), "wglSwapBuffer"), &hkwglSwapBuffers, reinterpret_cast<LPVOID*>(&twglSwapBuffers)) != MH_OK) return;
 }
